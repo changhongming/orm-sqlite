3c4f7ffbd2c673cd7a35c17d2e7b516047e9c73e
張宏銘 <m10613209@yuntech.edu.tw>
2019-03-17 11:58:16 +0800

    :sparkles: 導入sqlite型別列舉、根目錄套件引入

--- sqlClass/model.js
+++ sqlClass/model.js
@@ -1,8 +1,19 @@
 const SqlClass = require('./sql-class');
-const dbpaht = `${__dirname}/mydb.db` || ':memory:';
+const appRoot = require('app-root-path');
+const dbpaht = `${appRoot}/mydb.db` || ':memory:';
 class Model extends SqlClass {
   constructor(tablename, columns) {
     super(dbpaht, tablename, columns)
   }
 }
 module.exports = Model;
+
+module.exports.INTERGER = 'INTEGER';
+
+module.exports.TEXT = 'TEXT';
+
+module.exports.DATETIME = 'DATETIME';
+
+module.exports.BLOB = 'BLOB';
+
+module.exports.REAL = 'REAL';
